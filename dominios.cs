78d24544fbeb1a64bd8e1951db41aa32cce8a516
joaopirolo28 <joaopirolo28@gmail.com>
2026-05-14 23:24:18 -0300

    Start

---
+++ dominios.cs
@@ -0,0 +1,18 @@
+class Nome
+{
+    private string nome;
+    private bool validar(string nome)
+    {
+        return true;
+    }
+
+    public string getName()
+    {
+        return nome;
+    }
+    public bool setName(string nome)
+    {
+        return true;
+    }
+}
+
